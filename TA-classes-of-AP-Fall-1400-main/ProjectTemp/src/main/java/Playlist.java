7d639bd48b00d7cf5e9629cec39a483885a265e1
MobinHazar <mobinhazarkhani@gmail.com>
2022-07-05 21:54:42 +0430

    Initial PlayList file.

--- TA-classes-of-AP-Fall-1400-main/ProjectTemp/src/main/java/Playlist.java
+++ TA-classes-of-AP-Fall-1400-main/ProjectTemp/src/main/java/Playlist.java
@@ -1,2 +1,9 @@
 public class Playlist {
+    public String name;
+    public Track[] tracks;
+
+    public Playlist(String name, Track[] tracks) {
+        this.name = name;
+        this.tracks = tracks;
+    }
 }
