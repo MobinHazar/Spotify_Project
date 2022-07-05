352892442f007a2a9bc71a65d61cbf88060a291d
MobinHazar <mobinhazarkhani@gmail.com>
2022-07-06 02:21:15 +0430

    bulid sing up process class.

--- TA-classes-of-AP-Fall-1400-main/ProjectTemp/src/main/java/Main.java
+++ TA-classes-of-AP-Fall-1400-main/ProjectTemp/src/main/java/Main.java
@@ -7,8 +7,11 @@ import io.swagger.client.api.UsersApi;
 import io.swagger.client.auth.ApiKeyAuth;
 import io.swagger.client.auth.OAuth;
 import io.swagger.client.model.AuthLoginBody;
+import io.swagger.client.model.AuthSignupBody;
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
     public static final String MY_API_KEY = "5f718eaba2e175cde156d51671bf000969d0bad1";
@@ -61,6 +64,45 @@ public class Main {
             }
         }
     }
+    public static void singupProcess(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter username");
+        String username = input.next();
+        System.out.println("Enter password");
+        String password = input.next();
+        boolean flag = true;
+        while (flag){
+            Pattern p = Pattern.compile("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]{8,}$");
+            Matcher m = p.matcher(password);
+            if(m.find()){
+                flag = false;
+            }
+            else{
+                System.out.println("Password must contain at least one letter and one number");
+                System.out.println("Enter password");
+                password = input.next();
+            }
+        }
+        String token = "";
+        try {
+            AuthSignupBody authSignupBody = new AuthSignupBody();
+            authSignupBody.setUsername(username);
+            authSignupBody.setPassword(password);
+            token = authApi.signUp(authSignupBody).getToken();
+            currentUser = new User(username,password);
+            System.out.println("You Successfully logged in");
+            currentUser.token = token;
+            defaultClient.setAccessToken(currentUser.token);
+            OAuth bearerAuth = (OAuth) defaultClient.getAuthentication("bearerAuth");
+            bearerAuth.setAccessToken(currentUser.token);
+            userMenuProcess();
+            currentUser = new User(username, password);
+            currentUser.token = token;
+        } catch (ApiException apiException) {
+            System.out.println("This username is already taken plz try another one");
+            singupProcess();
+        }
+    }
 
     public static void userMenuProcess() {
         Scanner input = new Scanner(System.in);
@@ -107,6 +149,7 @@ public class Main {
 
         int choice = input.nextInt();
         if (choice == 1) loginProcess();
+        else if (choice == 2) singupProcess();
 
     }
 }
