f5280df0c99bfd3381135598dfb1f09dc7c9b6cf
MobinHazar <mobinhazarkhani@gmail.com>
2022-07-06 13:45:19 +0430

    some edit on menu.

--- TA-classes-of-AP-Fall-1400-main/ProjectTemp/src/main/java/Menu.java
+++ TA-classes-of-AP-Fall-1400-main/ProjectTemp/src/main/java/Menu.java
@@ -1,22 +1,30 @@
 import io.swagger.client.ApiException;
+import io.swagger.client.model.Playlists;
 
 import java.util.Scanner;
 
 public class Menu {
     public static long start = 0;
     public static String allTracks;
+    public static Playlists playlists;
     public static boolean isFirstCall = true;
+    public static String profile="";
     public static void userMenuProcess() {
         Scanner input = new Scanner(System.in);
-        System.out.println("1-Profile\n2-Tracks");
+        System.out.println("1-Profile\n2-Tracks\n3-Playlists\n4-Upgrade to premium\n5-Logout");
         int choice = input.nextInt();
         if (choice == 1) {
             long currentTime = System.currentTimeMillis() / 1000;
             if (currentTime - start > 20 || isFirstCall) {
                 try {
-                    System.out.println(Main.usersApi.getProfileInfo().toString());
+                    profile = Main.usersApi.getProfileInfo().toString();
+                    System.out.println(profile);
                     start = System.currentTimeMillis() / 1000;
+                    System.out.println("Enter 0 at any page to go back");
                     int exit = input.nextInt();
+                    while (exit != 0) {
+                        exit = input.nextInt();
+                    }
                     isFirstCall = false;
                     userMenuProcess();
                 } catch (ApiException apiException) {
@@ -25,20 +33,39 @@ public class Menu {
                 }
             } else {
                 //cached data
-                System.out.println();
+                System.out.println(profile);
             }
         } else if (choice == 2) {
             long currentTime = System.currentTimeMillis() / 1000;
-            if (currentTime - start > 20) {
+            if (currentTime - start > 20 || isFirstCall) {
                 try {
                     allTracks = Main.usersApi.getTracksInfo().toString();
                     System.out.println(allTracks);
                     start = System.currentTimeMillis() / 1000;
+                    isFirstCall = false;
+                    System.out.println("Enter 0 at any page to go back");
+                    int exit = input.nextInt();
+                    while (exit != 0) {
+                        exit = input.nextInt();
+                    }
+                    userMenuProcess();
                 } catch (ApiException apiException) {
+                    isFirstCall = false;
                     System.out.println(apiException.getResponseBody());
                 }
             } else {
                 System.out.println(allTracks);
+            }
+        }
+        else if (choice == 3){
+            long currentTime = System.currentTimeMillis() / 1000;
+            if (currentTime - start > 20 || isFirstCall) {
+                try {
+                    playlists = Main.usersApi.getPlaylistsInfo();
+            System.out.println(playlists);
+        } catch (ApiException apiException) {
+            System.out.println(apiException.getResponseBody());
+        }
             }
         }
     }
--- TA-classes-of-AP-Fall-1400-main/ProjectTemp/src/main/java/Playlist.java
+++ TA-classes-of-AP-Fall-1400-main/ProjectTemp/src/main/java/Playlist.java
@@ -1,10 +1,12 @@
 import io.swagger.client.model.Track;
 
 public class Playlist {
+    public String id;
     public String name;
     public Track[] tracks;
 
-    public Playlist(String name, Track[] tracks) {
+    public Playlist(String id, String name, Track[] tracks) {
+        this.id = id;
         this.name = name;
         this.tracks = tracks;
     }
