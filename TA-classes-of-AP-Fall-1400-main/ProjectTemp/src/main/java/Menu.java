25404c3499aa86b7c0dc0fe7e165e2b899a13a7a
MobinHazar <mobinhazarkhani@gmail.com>
2022-07-06 16:06:46 +0430

    build playlist process.

--- TA-classes-of-AP-Fall-1400-main/ProjectTemp/src/main/java/Menu.java
+++ TA-classes-of-AP-Fall-1400-main/ProjectTemp/src/main/java/Menu.java
@@ -1,5 +1,6 @@
 import io.swagger.client.ApiException;
 import io.swagger.client.model.Playlists;
+import io.swagger.client.model.PlaylistsBody;
 
 import java.util.Scanner;
 
@@ -56,17 +57,58 @@ public class Menu {
             } else {
                 System.out.println(allTracks);
             }
-        }
-        else if (choice == 3){
-            long currentTime = System.currentTimeMillis() / 1000;
-            if (currentTime - start > 20 || isFirstCall) {
-                try {
-                    playlists = Main.usersApi.getPlaylistsInfo();
-            System.out.println(playlists);
-        } catch (ApiException apiException) {
-            System.out.println(apiException.getResponseBody());
-        }
-            }
+        } else if (choice == 3) {
+            playlistProcess();
         }
     }
+    public static void playlistProcess(){
+        Scanner input = new Scanner(System.in);
+        long currentTime = System.currentTimeMillis() / 1000;
+        if (currentTime - start > 20 || isFirstCall) {
+            try {
+                playlists = Main.usersApi.getPlaylistsInfo();
+                System.out.println(playlists);
+                System.out.println("Enter 0 at any page to go back\n1- Create Playlist\n2- Add Track to Playlist\n3- Remove Track from Playlist\n4- Delete Playlist");
+                int choice2 = input.nextInt();
+                if (choice2 == 1) {
+                        PlaylistsBody playlistsBody = new PlaylistsBody();
+                        System.out.println("Enter Playlist Name");
+                        String name = input.next();
+                        playlistsBody.setName(name);
+                        int id = Main.usersApi.createPlaylist(playlistsBody).getId();
+                        System.out.println("Playlist created successfully with id: " + id);
+                        playlistProcess();
+                } else if (choice2 == 2) {
+                        System.out.println("Enter Playlist ID");
+                        int playlistId = input.nextInt();
+                        System.out.println("Enter Track ID");
+                        String trackId = input.next();
+                        Main.usersApi.addTrackToPlaylist(playlistId, trackId);
+                    System.out.println("Track added successfully");
+                        playlistProcess();
+
+                } else if (choice2 == 3) {
+                    System.out.println("Enter Playlist ID");
+                    int playlistId = input.nextInt();
+                    System.out.println("Enter Track ID");
+                    String trackId = input.next();
+                    Main.usersApi.removeTrackFromPlaylist(playlistId, trackId);
+                    System.out.println("Track removed successfully");
+                    playlistProcess();
+                } else if (choice2 == 4) {
+                    System.out.println("Enter Playlist ID");
+                    int playlistId = input.nextInt();
+                    System.out.println(Main.usersApi.deletePlaylist(playlistId));
+                    System.out.println("Playlist deleted successfully");
+                    playlistProcess();
+                }
+            } catch (ApiException apiException) {
+                System.out.println(apiException.getResponseBody());
+            }
+        } else {
+            System.out.println(playlists);
+        }
+
+    }
 }
+
--- TA-classes-of-AP-Fall-1400-main/ProjectTemp/src/main/java/Playlist.java
+++ TA-classes-of-AP-Fall-1400-main/ProjectTemp/src/main/java/Playlist.java
@@ -3,11 +3,9 @@ import io.swagger.client.model.Track;
 public class Playlist {
     public String id;
     public String name;
-    public Track[] tracks;
 
-    public Playlist(String id, String name, Track[] tracks) {
+    public Playlist(String id, String name) {
         this.id = id;
         this.name = name;
-        this.tracks = tracks;
     }
 }
